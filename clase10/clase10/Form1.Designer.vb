742788e7079641b2b553427c7f266c0018115142
alanfrancoa <alvarezalanfranco@gmail.com>
2024-05-28 01:44:36 -0300

    Se crea proyecto visto en clase 10.

---
+++ clase10/clase10/Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(439, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(468, 646)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(144, 254)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(134, 50)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Cargar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(79, 93)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(269, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(79, 134)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(269, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(79, 175)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(269, 20)
+        Me.TextBox3.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(76, 77)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Nombre"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(76, 118)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Apellido"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(76, 159)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(32, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Edad"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(56, 447)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(159, 20)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Promedio de edad:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(56, 514)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(212, 20)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Promedio de edad con M:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(308, 469)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(40, 20)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "0.00"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(308, 540)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(40, 20)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "0.00"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(922, 671)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+End Class
---
+++ clase10/clase10/Form1.vb
@@ -0,0 +1,90 @@
+﻿Public Class Form1
+
+#Region "Dependencias"
+    Private dtPersonas As DataTable
+#End Region
+
+#Region "Inicializar"
+    Private Sub InicializarDataTable()
+        Dim dt As New DataTable
+        dt.Columns.Add("Nombre")
+        dt.Columns.Add("Apellido")
+        dt.Columns.Add("Edad", GetType(Integer))
+        Me.SetDtPersonas(dt)
+    End Sub
+
+#End Region
+
+#Region "Acciones"
+    Private Sub SetDtPersonas(dt As DataTable)
+        Me.dtPersonas = dt
+        DataGridView1.DataSource = dt
+        'Apuntamos el DataTable al source del DataGridView1'
+    End Sub
+
+    Private Sub RecargarPromedio()
+        Dim acumulador As Double = 0
+        Dim acumuladorM As Double = 0
+        Dim contadorM As Integer = 0
+
+        For Each renglon As DataRow In dtPersonas.Rows
+
+            acumulador += renglon("Edad")
+            Dim nombre As String = renglon("Nombre")
+
+            'nombre toma el valor del renglon Nombre'
+            If nombre.ToUpper().StartsWith("M") Then
+                acumuladorM += renglon("Edad")
+                contadorM += 1
+            End If
+            'En caso de que Nombre empiece con M, lo acumula e incrementa el contadorM'
+
+            Dim promedio = acumulador / dtPersonas.Rows.Count
+            'Para todo elemento que sea parte de la coleccion, count nos va a dar la cantidad de elementos.'
+
+            Label6.Text = promedio.ToString("#0.00")
+            'Asignamos el valor de promedio en el label del Form. ToString 0.00, nos da los primeros dos digitos despues de la coma.'
+
+            If contadorM > 0 Then
+                Dim promedioM = acumuladorM / contadorM
+                Label7.Text = promedioM.ToString("#0.00")
+            Else
+                Label7.Text = "0.00"
+            End If
+
+        Next
+    End Sub
+
+
+#End Region
+
+#Region "Eventos"
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        InicializarDataTable()
+        Me.dtPersonas.Rows.Add("Juan", "Gomez", 44)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim nombre = TextBox1.Text
+        Dim apellido = TextBox2.Text
+        Dim edad = Integer.Parse(TextBox3.Text)
+        'Captura los valores de los textbox, en el ultimo lo convierte a Entero'
+
+        Me.dtPersonas.Rows.Add(nombre, apellido, edad)
+        'Agrega una fila en el data table, con las variables'
+
+        RecargarPromedio()
+        'Llamo a la funcion de muestra de promedios'
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        InicializarDataTable()
+    End Sub
+    'Cuando se carga el form, inicializa data table'
+
+
+#End Region
+
+End Class
